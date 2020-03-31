6d29c7f89a3d3767122d02d3ead22440495bdab8
Weronika Biela <weronika.biela@student.uj.edu.pl>
2020-03-31 12:40:48 +0200

    Raw file for preparation of TERXfiles

---
+++ functions/Preparation_of_TERX_files.py
@@ -0,0 +1,7 @@
+# TODO 1 rzutowanie wszystkich kolumn,
+#     2 zczytanie kalibracji wynikającej z pliku par
+#     3 Spectrum_Calibration
+
+
+def projection_of_TERX_file(dir_path='../data/terx'):
+    print('Projection done')
